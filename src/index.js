6fe13a491186a078239b54d1de8f38268b866d05
Richard Towers <richard_towers@yahoo.co.uk>
2017-10-15 15:08:43 +0100

    chore(paas): Prepare for Heroku

--- src/index.js
+++ src/index.js
@@ -21,4 +21,5 @@ function seventh (req, res) {
 app.get('/', sixth)
 app.get('/sixth', sixth)
 app.get('/seventh', seventh)
-app.listen(3000, () => console.log('Listening on 3000'))
+const port = process.env.PORT || 3000 
+app.listen(port, () => console.log(`Listening on ${port}`))
