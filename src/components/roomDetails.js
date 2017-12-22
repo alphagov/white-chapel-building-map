e8967658930b8c315241bb51fbfaff4741133a46
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-12-22 11:33:39 +0000

    Style room details and delete redundant code

--- src/components/roomDetails.js
+++ src/components/roomDetails.js
@@ -1,48 +1,15 @@
 const e = require('react').createElement
 
 const rooms = ({
-  6: [
-    {number: 609, status: "free"},
-    {number: 608, x:  60, y: 130, width:  50, height:  50},
-    {number: 610, x: 220, y:  60, width:  40, height:  60},
-    {number: 611, x: 260, y:  60, width:  40, height: 120},
-    {number: 612, x: 220, y: 120, width:  40, height:  60},
-    {number: 606, x:  60, y: 200, width:  35, height:  50},
-    {number: 607, x:  95, y: 200, width:  35, height:  50},
-    {number: 605, x:  60, y: 270, width:  30, height:  20},
-    {number: 604, x:  60, y: 290, width:  30, height:  20},
-    {number: 613, x: 270, y: 270, width:  30, height:  20},
-    {number: 614, x: 270, y: 290, width:  30, height:  20},
-    {number: 603, x:  60, y: 410, width:  30, height:  20},
-    {number: 602, x:  60, y: 430, width:  30, height:  20},
-    {number: 615, x: 270, y: 410, width:  30, height:  20},
-    {number: 616, x: 270, y: 430, width:  30, height:  20},
-    {number: 601, x:  60, y: 540, width:  50, height:  60},
-  ],
-  7: [
-    {number: 710, x:  60, y:  80, width:  50, height:  50},
-    {number: 709, x:  60, y: 130, width:  50, height:  50},
-    {number: 711, x: 220, y:  60, width:  40, height:  70},
-    {number: 712, x: 260, y:  60, width:  40, height: 120},
-    {number: 708, x:  60, y: 270, width:  30, height:  20},
-    {number: 707, x:  60, y: 290, width:  30, height:  20},
-    {number: 713, x: 270, y: 270, width:  30, height:  20},
-    {number: 714, x: 270, y: 290, width:  30, height:  20},
-    {number: 705, x:  60, y: 410, width:  30, height:  20},
-    {number: 704, x:  60, y: 430, width:  30, height:  20},
-    {number: 716, x: 270, y: 410, width:  30, height:  20},
-    {number: 717, x: 270, y: 430, width:  30, height:  20},
-    {number: 706, x:  60, y: 330, width:  40, height:  60},
-    {number: 715, x: 260, y: 330, width:  40, height:  60},
-    {number: 701, x:  60, y: 540, width:  50, height:  60},
-    {number: 703, x:  60, y: 620, width:  30, height:  30},
-    {number: 702, x:  90, y: 620, width:  30, height:  30},
-    {number: 720, x: 240, y: 640, width: 120, height:  80},
-    {number: 719, x: 220, y: 500, width:  80, height: 100},
-    {number: 718, x: 240, y: 480, width:  40, height:  20},
-  ]
+  6: ["601-616"],
+  7: ["701-720"]
 })
 module.exports = props => e('div', { className: 'room-details-container' },
   e('h2', { className: 'room-details-header' }, "Here be details"),
-  e('p', { className: 'room-details-status' }, "status: occupied")
+  e('p', { className: 'room-details-status' }, "status: occupied"),
+  e('ul', { className: 'room-details-schedule' }, [
+    "Schedule:",
+    e('li', null, "13:00 - 14:00 Next meeting"),
+    e('li', null, "14:00 - 15:00 Another meeting")
+  ])
 )
